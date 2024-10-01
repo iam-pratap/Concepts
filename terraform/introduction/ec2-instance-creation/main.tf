704881a5e6d46ce81bb253725873ba99f89e8d76
Honey Pratap <144008101+iam-pratap@users.noreply.github.com>
2024-10-01 09:27:45 +0530

    Update main.tf

--- terraform/introduction/ec2-instance-creation/main.tf
+++ terraform/introduction/ec2-instance-creation/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "first-infra" {
-  ami                     = "ami-08718895af4dfa033"
+  ami                     = "ami-08718895af4dfa033" # Set an appropriate AMI ID
   instance_type           = "t2.micro"
   
   tags = {
-    Name = "test-infra"
+    Name = "test-infra" # Set the tag to identify
   }
 }
