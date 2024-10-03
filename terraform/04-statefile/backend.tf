7a176c7714badc462efe5de266e3aaf55e0bb160
Honey Pratap <144008101+iam-pratap@users.noreply.github.com>
2024-10-03 10:30:26 +0530

    Update backend.tf

--- terraform/04-statefile/backend.tf
+++ terraform/04-statefile/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
-    key            = "abhi/terraform.tfstate"
-    region         = "us-east-1"
+    bucket         = "honey-s3-demo-statefile" # change this
+    key            = "honey/terraform.tfstate"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
