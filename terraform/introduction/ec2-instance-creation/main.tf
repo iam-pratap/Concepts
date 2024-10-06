a1053415bccc443fa8c2d2bcd41c24568bb5de8d
Honey Pratap <144008101+iam-pratap@users.noreply.github.com>
2024-10-06 14:11:36 +0530

    Delete terraform/introduction directory

--- terraform/introduction/ec2-instance-creation/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "first-infra" {
-  ami                     = "ami-06f855639265b5541" # Set an appropriate AMI ID
-  instance_type           = "t2.micro"
-  
-  tags = {
-    Name = "test-infra" # Set the tag to identify
-  }
-}
--- terraform/introduction/ec2-instance-creation/provider.tf
+++
@@ -1,4 +0,0 @@
-# Set your Cloud Provider name
-provider "aws" {
-     region = "ap-south-1"  # Set your desired AWS region
-   }
